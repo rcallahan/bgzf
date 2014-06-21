4782a79a9edf895c1ec2e74c816385ede97aad52
rcallahan <rcallahan@eurekagenomics.com>
2014-06-21 14:30:14 -0500

    Builds, need to play with zlib ffi

---
+++ Pipes/Bgzf.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+module Pipes.Bgzf where
+
+import Data.Streaming.Zlib.Lowlevel
+import Data.Streaming.Zlib (ZlibException (..), defaultWindowBits)
+import Foreign.Ptr
+import Foreign.ForeignPtr
+import Foreign.C.Types
+import Data.ByteString.Internal
+import System.IO.Unsafe
+import qualified Data.ByteString as B
+import Data.ByteString.Unsafe
+
+foreign import ccall unsafe "streaming_commons_free_z_stream_inflate"
+    freeZstream :: ZStream' -> IO ()
+
+bgzfBlockSize = 65536
+
+inflateBlock :: ByteString -> ByteString
+inflateBlock bs = unsafePerformIO $ do
+    zstr <- zstreamNew
+    inflateInit2 zstr defaultWindowBits
+    fp <- mallocByteString bgzfBlockSize
+    withForeignPtr fp $ \p -> do
+        let buff = castPtr p
+        c_set_avail_out zstr buff $ fromIntegral bgzfBlockSize
+        unsafeUseAsCStringLen bs $ \(cstr, len) -> do
+            c_set_avail_in zstr cstr $ fromIntegral len
+            res <- c_call_inflate_noflush zstr
+            avail <- c_get_avail_out zstr
+            freeZstream zstr
+            let size = bgzfBlockSize - fromIntegral avail
+            return $ PS fp 0 size
+
